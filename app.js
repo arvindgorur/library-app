6b6ed63a00e1f54c90b86e8cba7207b28a77a36d
Arvind Gorur <arvind.gorur@hotmail.com>
2018-05-01 16:05:40 -0400

    With CSS!

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var chalk   = require('chalk');
 var morgan  = require('morgan');
+var path    = require('path');
 
 var app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname,'/public')));
+app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname,'/node_modules/jquery/dist')));
 
 app.get('/', function(req,res) {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, function() {
