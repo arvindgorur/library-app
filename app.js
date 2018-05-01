e68092724d4dcd2e33d21dd2a1e0734d4a155b06
Arvind Gorur <arvind.gorur@hotmail.com>
2018-05-01 15:38:34 -0400

    Updated console outputs.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var chalk   = require('chalk');
+var morgan  = require('morgan');
 
 var app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/', function(req,res) {
     res.send('Hello World');
 });
 
 app.listen(3000, function() {
-    console.log('Listening on port 3000');
+    console.log(`Listening on port ${chalk.green('3000')}`);
 })
