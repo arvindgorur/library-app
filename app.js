d458001c50c766e6670269869fd308f4951567fe
Arvind Gorur <arvind.gorur@hotmail.com>
2018-05-10 14:21:08 -0400

    Some updates

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var path    = require('path');
 var app = express();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'/public')));
-app.use('/css', express.static(path.join(__dirname,'/css')));
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname,'/node_modules/jquery/dist')));
